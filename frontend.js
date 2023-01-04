b7d9f36ddfda2a23f3f4ac51f2c7b773db8b3a6a
Markas Vielavicius <maildetectionteam@gmail.com>
2023-01-05 00:10:12 +0200

    Made battleships frontend

--- frontend.js
+++ frontend.js
@@ -77,16 +77,16 @@ var fromAddress;
 
 
 
-
 async function test()
 {
+    console.log(fromAddress);
     response = await contract.methods.changeGreeting("bh hb").send(
         {
             from: fromAddress,
             gas: "21000"
           }
     );
-    alert(response);
+    console.log(response);
 }
 
 
@@ -98,7 +98,8 @@ async function greet() {
 async function connect()
 {
     if (typeof window.ethereum === "undefined") {
-        return setError("Metamask not installed");
+        alert("ERROR: Metamask is not installed, please install it firstly in browser")
+        return;
     }
 
   
@@ -116,4 +117,90 @@ async function connect()
             fromAddress = accounts[0];
         }
     });
+
+}
+
+
+function generateMap() {
+  clearMap();
+  coords = generateShips();
+  coords.forEach(shipPart => {
+    getShipChecker(shipPart.x, shipPart.y).checked = true;
+  });
+}
+
+function getShipChecker(x, y)
+{
+  var id = "field-x" + (Number(x) + 1) + "-y" + (Number(y) + 1);
+  field = document.getElementsByClassName(id);
+  shipField = field[1];
+  
+  return shipField.querySelector('.ship.check');
+}
+
+
+function clearMap(){
+  for (let x = 0; x < 10; x++) {
+    for (let y = 0; y < 10; y++) {
+      getShipChecker(x, y).checked = false;
+    }
+  }
+}
+
+
+function generateShips() {
+  // Array to store the ship locations
+  const ships = [];
+
+  // List of ship sizes (in number of cells)
+  const sizes = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
+
+  // Generate ship locations
+  for (const size of sizes) {
+    let x, y;
+    let orientation;
+
+    // Generate random position and orientation for the ship
+    // Keep generating until the ship does not overlap with any existing ships
+    // and fits within the grid
+    do {
+      orientation = Math.random() < 0.5 ? 'horizontal' : 'vertical';
+
+      if (orientation === 'horizontal') {
+        x = Math.floor(Math.random() * (10 - size));
+        y = Math.floor(Math.random() * 10);
+      } else {
+        x = Math.floor(Math.random() * 10);
+        y = Math.floor(Math.random() * (10 - size));
+      }
+    } while (x < 0 || y < 0 || x + size > 10 || y + size > 10 || shipOverlaps(size, x, y, orientation, ships));
+
+    // Add the ship cells to the ships array
+    for (let i = 0; i < size; i++) {
+      if (orientation === 'horizontal') {
+        ships.push({x: x + i, y});
+      } else {
+        ships.push({x, y: y + i});
+      }
+    }
+  }
+
+  return ships;
+}
+
+function shipOverlaps(size, x, y, orientation, ships) {
+  // Check if the ship overlaps with any of the existing ships
+  for (const cell of ships) {
+    if (orientation === 'horizontal') {
+      if (Math.abs(cell.y - y) <= 1 && cell.x >= x - 1 && cell.x < x + size + 1) {
+        return true;
+      }
+    } else {
+      if (Math.abs(cell.x - x) <= 1 && cell.y >= y - 1 && cell.y < y + size + 1) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
